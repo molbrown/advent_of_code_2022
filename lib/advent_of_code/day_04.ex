49d34782475f16e30911b54a3c9e1f78b39d3469
molbrown <molly@spreedly.com>
2022-12-04 14:52:18 -0500

    Day 4 Part 2

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -3,7 +3,8 @@ defmodule AdventOfCode.Day04 do
     Enum.count(input, fn [a, b] -> fully_contained(a, b) end)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    Enum.count(input, fn [a, b] -> !Range.disjoint?(a, b) end)
   end
 
   def fully_contained(a1..a2, b1..b2) do
--- lib/mix/tasks/d04.p2.ex
+++ lib/mix/tasks/d04.p2.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D04.P2 do
 
   @shortdoc "Day 04 Part 2"
   def run(args) do
-    input = nil
+    input = get_input()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
@@ -13,5 +13,16 @@ defmodule Mix.Tasks.D04.P2 do
         input
         |> part2()
         |> IO.inspect(label: "Part 2 Results")
+  end
+
+  defp get_input do
+    "../../data/day_04.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+    |> Stream.map(&String.split(&1, ["-", ","], trim: true))
+    |> Enum.to_list()
+    |> Enum.map(&Enum.map(&1, fn x -> Integer.parse(x) end))
+    |> Enum.map(fn [{a, _}, {b, _}, {c, _}, {d, _}] -> [a..b, c..d] end)
   end
 end

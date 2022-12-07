59fb777fb8db83b276532e188dce3f8bd3416881
molbrown <molly@spreedly.com>
2022-12-06 22:26:14 -0500

    Day 6 Part 2

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -7,14 +7,19 @@ defmodule AdventOfCode.Day06 do
     |> Kernel.+(1)
   end
 
-  def part2(_args) do
-  end
-
-  def start_of_packet(stream, index) when index < 3 do
-    false
+  def part2(input) do
+    Enum.with_index(input)
+    |> Enum.find(fn {x, i} -> start_of_message(input, i) end)
+    |> Tuple.to_list()
+    |> Enum.at(1)
+    |> Kernel.+(1)
   end
 
   def start_of_packet(stream, index) do
-    length(Enum.uniq([Enum.at(stream, index - 3), Enum.at(stream, index - 2), Enum.at(stream, index - 1), Enum.at(stream, index)])) == 4
+    length(Enum.uniq(Enum.slice(stream, index - 3, 4))) == 4
+  end
+
+  def start_of_message(stream, index) do
+    length(Enum.uniq(Enum.slice(stream, index - 13, 14))) == 14
   end
 end

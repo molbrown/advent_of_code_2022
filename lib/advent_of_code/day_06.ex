3e7aac0896c47c0a7755f0cae40b56b2092dcfc0
molbrown <molly@spreedly.com>
2022-12-06 22:13:23 -0500

    Day 6 Part 1

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    Enum.with_index(input)
+    |> Enum.find(fn {x, i} -> start_of_packet(input, i) end)
+    |> Tuple.to_list()
+    |> Enum.at(1)
+    |> Kernel.+(1)
   end
 
   def part2(_args) do
+  end
+
+  def start_of_packet(stream, index) when index < 3 do
+    false
+  end
+
+  def start_of_packet(stream, index) do
+    length(Enum.uniq([Enum.at(stream, index - 3), Enum.at(stream, index - 2), Enum.at(stream, index - 1), Enum.at(stream, index)])) == 4
   end
 end
--- lib/mix/tasks/d06.p1.ex
+++ lib/mix/tasks/d06.p1.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D06.P1 do
 
   @shortdoc "Day 06 Part 1"
   def run(args) do
-    input = nil
+    input = get_input()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
@@ -13,5 +13,14 @@ defmodule Mix.Tasks.D06.P1 do
         input
         |> part1()
         |> IO.inspect(label: "Part 1 Results")
+  end
+
+  def get_input do
+    "../../data/day_06.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.codepoints()/1)
+    |> Enum.to_list()
+    |> List.first()
   end
 end
